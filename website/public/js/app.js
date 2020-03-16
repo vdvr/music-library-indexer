b9e78acea44cacb36686d7f489d42913a0c52958
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-03-16 15:16:25 +0100

    init

--- website/public/js/app.js
+++ website/public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vdvr/Documents/Sites/music-library-indexer/website/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vdvr/Documents/Sites/music-library-indexer/website/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\faust\Documents\School\2 ba\ICT Project\shared-homestead\music-library-indexer\website\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\faust\Documents\School\2 ba\ICT Project\shared-homestead\music-library-indexer\website\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
